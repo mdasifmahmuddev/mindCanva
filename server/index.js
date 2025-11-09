c4243274fd9c07f06e530edf13e3aa9b5fffa81b
mdasifmahmuddev <md.asif.mahmud.dev@gmail.com>
2025-11-10 04:09:40 +0600

    mongodb connection with users database post req && artworks get req check

--- server/index.js
+++ server/index.js
@@ -1,28 +1,16 @@
+require('dotenv').config();
 
-require('dotenv').config()
-
-
-const express = require('express')
-const app = express()
-const port = 3000
-var cors = require('cors')
-//mongo
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+app.use(cors());
+app.use(express.json());
 
-//cors : security purpose . 
-//express : express js a framework which control get , update , delete etc req. 
-//node js : node js is a runtime which run server by js . 
-
-app.use(cors())
-app.use(express.json())
-
-
-//mongodb connection information  
-//install dotenev for take password from .env 
 const uri = `mongodb+srv://asif:${process.env.MONGO_PASSWORD}@mindcanvas.ah8atu6.mongodb.net/?appName=mindCanvas`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -33,37 +21,39 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    //art database connection 
     const database = client.db("mindcanva-db");
     const artworks = database.collection("artworks");
+    const userCollection = database.collection('users');
 
-    //find() and findone() =find for all and findone() for specific 
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        res.send({ message: 'user already exists', insertedId: null });
+      } else {
+        const result = await userCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
 
     app.get('/artworks', async (req, res) => {
-
       const result = await artworks.find().toArray();
+      res.send(result);
+    });
 
-       res.send(result)
-})
-
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-   //  await client.close(); : this cause error . 
   }
 }
+
 run().catch(console.dir);
 
-
-
- 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
